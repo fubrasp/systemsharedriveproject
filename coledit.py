5505583497f11daf115ecd6e45dcba84a941e3da
Guillaume BERTRAND <guillaume5524@hotmail.fr>
2016-12-27 10:26:37 +0100

    merge client and server files delete previous files

--- coledit.py
+++ coledit.py
@@ -5,6 +5,8 @@
 #we don t need to re import constants.py which is already import in tool.py
 from tool import *
 from coleditargs import *
+import socket
+
 # USAGE
 #coledit -p <pseudo> -d <document>
 #use the arguments define in coleditargs
@@ -20,8 +22,21 @@ print(serverRunningresult)
 print("TEST RESULTAT DE L'AUTHENTIFICATION")
 print(authentificationResult)
 
-print("TEST EDITION FICHIER")
-editFile(arguments[document])
+#vient du code modifie de l'exemple
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("", 1111))
+
+print("Le nom du fichier surlequel voulez travailler:")
+file_name = input(">> ")
+s.send(file_name.encode())
+file_name = 'data/%s' % (file_name,)
+r = s.recv(9999999)
+with open(file_name,'wb') as _file:
+    _file.write(r)
+print("Le fichier a été correctement créer ou ouvert dans : %s." % file_name)
+
+#print("TEST EDITION FICHIER")
+#editFile(arguments[document])
 
 
 
