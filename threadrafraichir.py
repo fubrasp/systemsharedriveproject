fda5ca16f69e379afa77c894ec906cba5a686c22
Guillaume BERTRAND <guillaume5524@hotmail.fr>
2016-12-30 14:26:43 +0100

    comment threadrafraichir

--- threadrafraichir.py
+++ threadrafraichir.py
@@ -4,6 +4,8 @@ import os
 
 # 1 thread s'éxécute en continue avec le client pour permettre de détecter tout changement (sans bloquer le programme) sur le document et effectuer ainsi le refresh sur la vue client
 class RafraichirClientThread(threading.Thread):
+
+    # Constructeur du thread
     def __init__(self, sock, document):
         threading.Thread.__init__(self)
         self.sock = sock
@@ -11,6 +13,7 @@ class RafraichirClientThread(threading.Thread):
         self._stop = threading.Event()
         print("=> Vous allez travailler sur " + self.document)
 
+    # Méthode run du thread
     def run(self):
         stop=False
         while (True and (stop==False)):
@@ -25,8 +28,10 @@ class RafraichirClientThread(threading.Thread):
                 os.system('exit')
                 self.stop()
 
+    # Méthode permettant d'arrêter le thread
     def stop(self):
         self._stop.set()
 
+    # Méthode retournant l'état de l'attribut _stop
     def stopped(self):
         return self._stop.isSet()
